623e28ca99b113bccc60d6c03c4e310421425bf6
steve <551619855@qq.com>
2018-05-26 18:41:58 +0800

    Initial commit

---
+++ bin/ImageMatch.cpp
@@ -0,0 +1,49 @@
+/** 
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+*/
+//
+// Created by steve on 18-5-26.
+//
+
+#include <iostream>
+
+
+#include <opencv2/opencv.hpp>
+
+#include <opencv2/highgui.hpp>
+
+int main()
+{
+	std::cout << "main test" << std::endl;
+
+	cv::namedWindow("show");
+
+	cv::Mat src_img;
+	src_img = cv::imread("/home/steve/Pictures/37831.jpg");
+
+	while(1){
+		cv::waitKey(1);
+		cv::imshow("show",src_img);
+	}
+
+
+}
