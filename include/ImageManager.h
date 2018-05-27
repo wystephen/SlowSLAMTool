a1dc34015659c5dc8fc449d5e1c05db89a036fcd
steve <551619855@qq.com>
2018-05-27 15:07:17 +0800

    test serialization.

--- include/ImageManager.h
+++ include/ImageManager.h
@@ -5,9 +5,27 @@
 #ifndef SLOWSLAMTOOL_IMAGEMANAGER_H
 #define SLOWSLAMTOOL_IMAGEMANAGER_H
 
+#include <vector>
+#include <map>
+
 #include <boost/serialization/serialization.hpp>
 
+#include <opencv2/opencv.hpp>
+
 class ImageManager {
+public:
+	ImageManager();
+
+private:
+
+
+	std::map<int, std::string> image_name_map; // image id -> image name
+
+	std::map<int , cv::Mat> image_map; //image id -> image(opencv mat)
+
+
+
+
 
 };
 
